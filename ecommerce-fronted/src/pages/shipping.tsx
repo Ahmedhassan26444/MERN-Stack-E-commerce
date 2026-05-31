6c928b6968868a89afa611522ec2357d9a301c50
Ahmad <ahmedhassan.26444@gmail.com>
2026-06-01 01:07:35 +0500

    minor changes in project

--- ecommerce-fronted/src/pages/shipping.tsx
+++ ecommerce-fronted/src/pages/shipping.tsx
@@ -1,61 +1,67 @@
-import { useState } from "react"
-import { BiArrowBack } from 'react-icons/bi'
+import { useEffect, useState } from "react";
+import { BiArrowBack } from "react-icons/bi";
 import type { ChangeEvent } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const Shipping = () => {
+  const navigate = useNavigate();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { cartItems } = useSelector((state: any) => state.cartReducer);
 
-const navigate = useNavigate()
+  const [shippingInfo, setShippingInfo] = useState({
+    address: "",
+    city: "",
+    state: "",
+    country: "",
+    pinCode: "",
+  });
 
-const [shippingInfo, setShippingInfo] = useState({
-        address: "",
-        city: "",
-        state: "",
-        country: "",
-        pinCode: "",
-    });
+  const changeHandler = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setShippingInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
-const changeHandler = (
-  e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
-) => {
-  setShippingInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  useEffect(() => {
+    if (cartItems.length <= 0) return navigate("/cart");
+  }, [cartItems, navigate]);
+
+  return (
+    <div className="shipping">
+      <button className="back-btn" onClick={() => navigate("/cart")}>
+        <BiArrowBack />
+      </button>
+
+      <form>
+        <h1>Shipping Address</h1>
+
+        <input required type="text" placeholder="Address"
+          name="address" value={shippingInfo.address}
+          onChange={changeHandler} />
+
+        <input required type="text" placeholder="City"
+          name="city" value={shippingInfo.city}
+          onChange={changeHandler} />
+
+        <input required type="text" placeholder="State"
+          name="state" value={shippingInfo.state}
+          onChange={changeHandler} />
+
+        <select name="country" required value={shippingInfo.country}
+          onChange={changeHandler}>
+          <option value="">Choose Country</option>
+          <option value="india">India</option>
+        </select>
+
+        <input required type="number" placeholder="Pin Code"
+          name="pinCode" value={shippingInfo.pinCode}
+          onChange={changeHandler} />
+
+        <button type="submit">Pay Now</button>
+      </form>
+    </div>
+  );
 };
-     
-return  <div className="shipping">
-            <button className="back-btn" onClick={() =>navigate("/cart")}>
-                <BiArrowBack/>
-            </button>
 
-            <form >
-                <h1>
-                    Shipping Address
-                </h1>
-                <input required type="text" placeholder="Address" 
-                name="adress" value={shippingInfo.address} 
-                onChange={changeHandler} />
-
-                 <input required type="text" placeholder="city" 
-                name="city" value={shippingInfo.city} 
-                onChange={changeHandler} />
-
-                 <input required type="text" placeholder="state" 
-                name="state" value={shippingInfo.state} 
-                onChange={changeHandler} />
-
-                <select name="country" required value={shippingInfo.country} 
-                onChange={changeHandler}>
-                    <option value="">Choose Country</option>
-                    <option value="india">India</option>
-                    
-
-                </select>
-
-                 <input required type="number" placeholder="Pin Code" 
-                name="pinCode" value={shippingInfo.pinCode} 
-                onChange={changeHandler} />
-                <button type="submit">Pay Now</button>
-            </form>
-        </div>
-  
-}
-
-export default Shipping
+export default Shipping;
